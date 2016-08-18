c502dd699a6ab650754d63992140deb332116b61
syn2083 <syndicate2083@gmail.com>
2016-08-18 08:12:25 -0400

    Added folder handler skeleton and threaded initialization test

--- APTDemo/views.py
+++ APTDemo/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from .folder_monitor import folder_monitor_handler as fmh
+import threading
+import time
+
+threads = []
+
+
+def worker():
+    observer = fmh.Observer()
+    observer.schedule(fmh.FolderHandler(), path='C:\APTApplication\Server\Inputs\JIFAcks')
+    observer.start()
+    print('observer started')
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
 
 
 def demo_central(request):
